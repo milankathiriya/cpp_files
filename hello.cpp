7767bbaebee542027769b190877b26b5f175de9a
Milan Kathiriya <milankathiriya@Milans-MacBook-Pro-2.local>
2024-01-18 16:15:39 +0530

    first upload

---
+++ hello.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// dynamic program
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    cout << "Enter value of a: ";
+    cin >> a;
+
+    cout << "Enter value of b: ";
+    cin >> b;
+
+    c = a + b;
+
+    cout << "Sum is " << c;
+    cout << endl
+         << "Bye, see you soon..." << endl;
+
+    return 0;
+}
